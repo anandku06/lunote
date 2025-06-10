60cbaf5a385e279dcc9ca1db0d6934455c6083ce
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-11 02:06:32 +0530

    Configure MongoDB connection using environment variable and set up dotenv for environment management

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import e from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = e();
 
