453b4419c50563112c737f1fe0d92e6a8cff7275
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-12 00:08:20 +0530

    Refactor note controller methods to use async/await and improve error handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,9 @@ const app = e();
 const PORT = process.env.PORT || 5001
 connectDB()
 
+// Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
+app.use(e.json())
+
 app.use("/api/notes", noteRoutes);
 
 app.listen(PORT, () => {
