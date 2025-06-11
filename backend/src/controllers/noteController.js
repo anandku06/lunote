56e4f792bbfc7d05f80625499a3487bfd0eb15b9
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-12 01:38:49 +0530

    Add getNoteById route to get specific note

--- backend/src/controllers/noteController.js
+++ backend/src/controllers/noteController.js
@@ -2,10 +2,21 @@ import Note from "../models/Note.js";
 
 export const getAllNotes = async (req, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({createdAt : -1});
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getNoteById = async (req, res) => {
+  try {
+    const note = await Note.findById(req.params.id);
+    if (!note) return res.status(404).json({ message: "Note not found!!" });
+    res.json(note);
+  } catch (error) {
+    console.error("Error in getNoteById controller!!", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
@@ -47,9 +58,9 @@ export const deleteNote = async (req, res) => {
     if (!deletedNote)
       return res.status(404).json({ message: "Note not found!!" });
 
-    res.json({message : "Note deleted successfully!!"})
+    res.status(200).json({ message: "Note deleted successfully!!" });
   } catch (error) {
-    console.log("Error in deleteNote controller!!", error);
+    console.error("Error in deleteNote controller!!", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/noteRoutes.js
+++ backend/src/routes/noteRoutes.js
@@ -4,11 +4,13 @@ import {
   deleteNote,
   getAllNotes,
   updateNote,
+  getNoteById
 } from "../controllers/noteController.js";
 
 const router = e.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
