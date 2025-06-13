394fbf52edc9203d5c27e2973b8a9e18fda9f1b0
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-14 00:50:09 +0530

    backend finishing with some rate limiting setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,28 @@
 import e from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = e();
-const PORT = process.env.PORT || 5001
-connectDB()
+const PORT = process.env.PORT || 5001;
 
 // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
-app.use(e.json())
+app.use(e.json());
 
+app.use(rateLimiter);
 // our simple custom middleware
-app.use((req, res, next) => {
-  console.log(`Req method is ${req.method} and Req URL is ${req}`);
-  next()
-})
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} and Req URL is ${req}`);
+//   next()
+// })
 
 app.use("/api/notes", noteRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 });
