f079ac0627cd56edde08b82b9761e2b687a3b0e3
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-16 23:47:22 +0530

    feat: integrate Tailwind CSS and react-hot-toast for enhanced styling and notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <button onClick={() => toast.success("Congratulations")}>Click Me!!</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
