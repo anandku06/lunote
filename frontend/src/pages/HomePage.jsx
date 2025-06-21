f38545b0f5c277bda7d0150e1925aa926b419efd
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-06-21 19:49:05 +0530

    feat: implement NoteCard component and integrate it into HomePage for displaying notes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import NoteCard from "../components/NoteCard";
 import RateLimitUI from "../components/RateLimitUI";
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -13,7 +14,6 @@ const HomePage = () => {
     const fetchNotes = async () => {
       try {
         const res = await axios.get("http://localhost:5001/api/notes");
-        console.log(res.data);
         setNotes(res.data);
         setIsRateLimited(false);
       } catch (error) {
@@ -34,6 +34,21 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">
+            Loading notes...
+          </div>
+        )}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
