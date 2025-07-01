74ce34d8c5926da6eb44cc4e0c164029914b5eb9
anandku06 <161126335+anandku06@users.noreply.github.com>
2025-07-01 22:31:12 +0530

    feat: update server configuration and improve axios base URL handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import e from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-
+import cors from "cors";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -10,21 +10,28 @@ dotenv.config();
 
 const app = e();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Returns middleware that only parses json and only looks at requests where the Content-Type header matches the type option.
-app.use(cors({
-  origin : "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+}
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(e.json());
 app.use(rateLimiter);
 
-// our simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} and Req URL is ${req}`);
-//   next()
-// })
-
 app.use("/api/notes", noteRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(e.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
